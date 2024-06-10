f1b484ba9f8ab43b9388c4c33c800a7d024a245b
Steven Goldsmith <sgoldsmith@sonatype.com>
2024-06-10 19:19:44 -0400

    Waits for Nexus to start, chang admin password and add license.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # nexus-ldap
 Docker compose for Nexus 3 and ldap
+
+This assumes you have Docker Compose installed. I use Docker Desktop and Ubuntu in WLS2.
+
+* Edit [docker-compose.yaml](docker-compose.yaml) as needed.
+* `cd scripts`
+*  ./nexus-config.sh
+
+```sgoldsmith@sonatype:/mnt/d/IdeaProjects/nexus-ldap/scripts$ ./nexus-config.sh
+[+] Running 3/3
+ ✔ Network nexus-ldap_default      Created                                                             0.0s
+ ✔ Volume "nexus-ldap_nexus-data"  Created                                                             0.0s
+ ✔ Container nexus-ldap            Started                                                             0.4s
+Waiting for Nexus to start.....
+Nexus up
+Changing admin password
+Password changed
+License /mnt/c/Users/StevenGoldsmith/Documents/2023-sonatype-internal-rm-lc-fw-fwfa-adp-alp-iacp-5000apps-1000rm_users-1000lc_users-1000fw_users.lic installed```
--- scripts/nexus-config.sh
+++ scripts/nexus-config.sh
@@ -1,24 +1,47 @@
 #!/bin/bash
 
-CONTAINER_NAME="nexus-ldap-nexus-1"
-PASSWORD_FILE="sonatype-work/nexus3/admin.password"
+container_name="nexus-ldap"
+password_file="sonatype-work/nexus3/admin.password"
+license_file="/mnt/c/Users/StevenGoldsmith/Documents/2023-sonatype-internal-rm-lc-fw-fwfa-adp-alp-iacp-5000apps-1000rm_users-1000lc_users-1000fw_users.lic"
+
+docker-compose -f ../docker-compose.yaml up -d
 
 # Check if the container is running
-if [ "$(docker inspect "$CONTAINER_NAME" --format '{{.State.Status}}')" = "running" ]; then
+if [ "$(docker inspect "$container_name" --format '{{.State.Status}}')" = "running" ]; then
+  # Wait for Nexus to start up
+  max_attempts=20
+  attempt=0
+  printf 'Waiting for Nexus to start'
+  while [[ "$attempt" -lt "$max_attempts" ]]; do
+    printf '.'
+    result=$(curl -o /dev/null -s -w "%{http_code}\n" -X GET http://localhost:8081/service/rest/v1/status)
+    if [[ "$result" == "200" ]]; then
+        printf "\nNexus up\n"
+        break
+    fi
+    sleep 10
+    ((attempt++))
+  done
+
   # Check if password file exists
-  if docker exec "$CONTAINER_NAME" test -f "$PASSWORD_FILE"; then
+  if docker exec "$container_name" test -f "$password_file"; then
     # Change admin password to admin123
-    echo "Changing admin password"
-    password=$(docker exec "$CONTAINER_NAME" cat "$PASSWORD_FILE")
+    printf "Changing admin password\n"
+    password=$(docker exec "$container_name" cat "$password_file")
     # REST call to change admin password
-    curl -v -u admin:"$password" -X 'PUT' \
-    "http://localhost:8081/service/rest/v1/security/users/admin/change-password" \
-    -H 'accept: application/json' \
-    -H 'Content-Type: text/plain' \
-    -d 'admin123'
+    result=$(curl -o /dev/null -s -w "%{http_code}\n" -u admin:"$password" -X 'PUT' "http://localhost:8081/service/rest/v1/security/users/admin/change-password" -H 'accept: application/json' -H 'Content-Type: text/plain' -d 'admin123')
+    if [[ "$result" == "204" ]]; then
+      printf "Password changed\n"
+      result=$(curl -o /dev/null -s -w "%{http_code}\n" -u admin:admin123 -H "Accept: application/json" -H "Content-Type: application/octet-stream" --data-binary "@$license_file" "http://localhost:8081/service/rest/v1/system/license")
+      if [[ "$result" == "200" ]]; then
+       printf "License %s installed\n" "$license_file"
+      fi
+    else
+      printf "Password not changed\n"
+    fi
   else
-    echo "admin.password does not exist"
+    printf "admin.password does not exist\n"
   fi
 else
-  echo "$CONTAINER_NAME not running"
+  printf "%s not running\n" "$container_name"
 fi
